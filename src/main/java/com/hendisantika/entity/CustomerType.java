8f4f9a5923898be20c98f80c0f63d82e1cac1772
Hendi Santika <hendisantika@yahoo.co.id>
2022-03-21 21:21:23 +0700

    #5 - Add CustomerType

---
+++ src/main/java/com/hendisantika/entity/CustomerType.java
@@ -0,0 +1,15 @@
+package com.hendisantika.entity;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-jpa-inheritance
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 21/03/22
+ * Time: 21.21
+ * To change this template use File | Settings | File Templates.
+ */
+public enum CustomerType {
+    INDIVIDUAL, LEGAL
+}
