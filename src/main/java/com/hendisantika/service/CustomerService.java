a997d5048466f01dde6ba58b5b9d498893a96c0f
Hendi Santika <hendisantika@yahoo.co.id>
2022-03-21 21:25:24 +0700

    #10 - Add CustomerService

---
+++ src/main/java/com/hendisantika/service/CustomerService.java
@@ -0,0 +1,26 @@
+package com.hendisantika.service;
+
+import com.hendisantika.repository.CustomerRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-jpa-inheritance
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 21/03/22
+ * Time: 21.25
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+@Transactional
+public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+}
