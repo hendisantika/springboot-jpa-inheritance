bcb427053be4af2560be6e9ca95ec17bbac249aa
Hendi Santika <hendisantika@yahoo.co.id>
2022-03-21 21:33:31 +0700

    #28 - Add storeCustomerTest in CustomerIntTest

--- src/test/java/com/hendisantika/CustomerIntTest.java
+++ src/test/java/com/hendisantika/CustomerIntTest.java
@@ -1,18 +1,28 @@
 package com.hendisantika;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hendisantika.controller.CustomerController;
+import com.hendisantika.entity.Customer;
 import com.hendisantika.repository.CustomerRepository;
 import com.hendisantika.service.CustomerService;
 import javafx.application.Application;
+import org.junit.jupiter.api.Test;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.PostConstruct;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * Created by IntelliJ IDEA.
@@ -47,5 +57,26 @@ public class CustomerIntTest {
         mockMvc = MockMvcBuilders
                 .standaloneSetup(customerController)
                 .build();
+    }
+
+    @Test
+    @Transactional
+    public void storeCustomerTest() throws Exception {
+        Customer customer = createCustomer();
+        MvcResult mvcResult = mockMvc
+                .perform(post(API)
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .content(new ObjectMapper().writeValueAsBytes(customer))
+                )
+                .andExpect(status().isCreated())
+                .andReturn();
+
+        Customer storedCustomer = new ObjectMapper().readValue(
+                mvcResult.getResponse().getContentAsString(), Customer.class
+        );
+        assertThat(storedCustomer.getValidFrom()).isEqualTo(customer.getValidFrom());
+        assertThat(storedCustomer.getValidTo()).isEqualTo(customer.getValidTo());
+        assertThat(storedCustomer.getSourceIdentifier()).isEqualTo(customer.getSourceIdentifier());
+        assertThat(storedCustomer.getActive()).isEqualTo(customer.getActive());
     }
 }
