5fa003a11ea28f9335083c41ee5e625cebccc692
Hendi Santika <hendisantika@yahoo.co.id>
2022-03-21 21:29:03 +0700

    #19 - Add store API in CustomerController

--- src/main/java/com/hendisantika/controller/CustomerController.java
+++ src/main/java/com/hendisantika/controller/CustomerController.java
@@ -1,9 +1,16 @@
 package com.hendisantika.controller;
 
+import com.hendisantika.entity.Customer;
 import com.hendisantika.service.CustomerService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 /**
  * Created by IntelliJ IDEA.
@@ -28,5 +35,20 @@ public class CustomerController {
      */
     public CustomerController(CustomerService customerService) {
         this.customerService = customerService;
+    }
+
+    /**
+     * Storing new customer | POST: /api/customer
+     *
+     * @param customer customer payload
+     * @return ResponseEntity with Customer as response body and http status code 201 | 400
+     */
+    @PostMapping
+    public ResponseEntity<Customer> store(@Valid @RequestBody Customer customer) {
+        if (customer.getId() != null) {
+            return ResponseEntity.badRequest().body(customer);
+        }
+        Customer storedCustomer = customerService.store(customer);
+        return new ResponseEntity<>(storedCustomer, HttpStatus.CREATED);
     }
 }
