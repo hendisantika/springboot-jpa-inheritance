58d52f8cd472a80157727803843044ff6de25032
Hendi Santika <hendisantika@yahoo.co.id>
2022-03-21 21:26:38 +0700

    #14 - Add ProductService

---
+++ src/main/java/com/hendisantika/service/ProductService.java
@@ -0,0 +1,26 @@
+package com.hendisantika.service;
+
+import com.hendisantika.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-jpa-inheritance
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 21/03/22
+ * Time: 21.26
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+@Transactional
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+}
